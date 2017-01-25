3e1ff8fb262d80f94d167bdc505cc55fcf82f42b
Agata Borkowska <borkowska.agata37@gmail.com>
2017-01-25 18:48:51 +0000

    Adding error code

--- sudoku_solver.c
+++ sudoku_solver.c
@@ -9,15 +9,23 @@ FILE *input_file;
 
 
 int main(int argc, char *argv[]) {
+  int error = 0;
+  while (error == 0) {
+    int *board = (int *) malloc(sizeof(int) * 81);
+    // record input
+    char buff[255];
+    input_file = fopen(argv[0], "r");
+    if (input_file == NULL) {
+	  printf("invalid file");
+	  error = 1;
+    }  
   
-  int *board = (int *) malloc(sizeof(int) * 81);
-  // record input
-  char buff[255];
-  input_file = fopen(argv[0], "r");
-  fscanf(input_file, "%s", buff);
-  printf(input_file,"1. - %s\n", buff);
+    fscanf(input_file, "%s", buff);
+    printf("1. - %s\n", buff);
 
-  // perform dfs
-    
+    // perform dfs
+    break;
+  }
+  return error;
 
 }
