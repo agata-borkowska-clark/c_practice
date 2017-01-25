7f18262a36745cc6403826b548f397fbb6766e60
Agata Borkowska <borkowska.agata37@gmail.com>
2017-01-25 19:03:38 +0000

    Fixed file reading test

--- sudoku_solver.c
+++ sudoku_solver.c
@@ -7,25 +7,23 @@
 FILE *input_file;
 
 
+int error = 0;
 
 int main(int argc, char *argv[]) {
-  int error = 0;
-  while (error == 0) {
     int *board = (int *) malloc(sizeof(int) * 81);
     // record input
     char buff[255];
-    input_file = fopen(argv[0], "r");
+    input_file = fopen(argv[1], "r");
     if (input_file == NULL) {
 	  printf("invalid file");
-	  error = 1;
+	  return 1;
     }  
   
     fscanf(input_file, "%s", buff);
     printf("1. - %s\n", buff);
 
     // perform dfs
-    break;
-  }
-  return error;
+    
+    return 0;
 
 }
